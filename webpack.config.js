69e4b391930b6f21b15087fd15a5d54a4e1e62dc
Paul <prose0021@gmail.com>
2020-02-18 16:56:00 -0800

    fix background sizing

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,6 @@ module.exports = {
     filename: "bundle.js"
   },
   devtool: "source-map",
-  // module: {
-  //     loaders: [
-  //         {
-  //             test: [/\.jsx?$/, /\.js?$/],
-  //             exclude: /(node_modules|bower_components)/,
-  //             loader: 'babel',
-  //             query: {
-  //                 presets: ['es2015']
-  //             }
-  //         }
-  //     ]
-  // },
   resolve: {
     extensions: ["*", ".js"]
   }
